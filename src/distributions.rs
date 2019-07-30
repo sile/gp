515c6f831c4eb0763ac3fa70d041d30bb2f2418d
Takeru Ohta <phjgt308@gmail.com>
2019-07-30 22:44:52 +0900

    Implement Normal::sample method.

--- src/distributions.rs
+++ src/distributions.rs
@@ -1,11 +1,38 @@
-use super::{Covariance, Means};
+use super::{Covariance, Matrix, Means, Vector};
+use nalgebra::base::dimension::{Dim, Dynamic, U1};
+use nalgebra::base::VecStorage;
 use rand::distributions::Distribution;
+use rand::Rng;
+use rand_distr::StandardNormal;
 
 /// https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Drawing_values_from_the_distribution
 #[derive(Debug)]
-pub struct Normal {}
+pub struct Normal {
+    means: Means,
+    covariance_l: Matrix,
+}
 impl Normal {
-    pub fn new(means: Means, covariance: Covariance) -> Self {
-        panic!()
+    pub fn new(means: Means, covariance: Covariance) -> Option<Self> {
+        let covariance_l = covariance.cholesky()?.l();
+        Some(Self {
+            means,
+            covariance_l,
+        })
     }
 }
+impl Distribution<Vector> for Normal {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Vector {
+        let n = self.means.as_slice().len();
+        let z = Vector::from_data(VecStorage::new(
+            Dynamic::from_usize(n),
+            U1,
+            StandardNormal.sample_iter(rng).take(n).collect::<Vec<_>>(),
+        ));
+        self.means.as_inner().clone() + self.covariance_l.clone() * z
+    }
+}
+
+// #[derive(Debug)]
+// pub struct Prior{
+
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use nalgebra;
-use nalgebra::base::dimension::Dynamic;
+use nalgebra::base::dimension::{Dynamic, U1};
 use nalgebra::base::VecStorage;
 use nalgebra::linalg::Cholesky;
 
 pub mod distributions;
 
 pub type Matrix = nalgebra::base::Matrix<f64, Dynamic, Dynamic, VecStorage<f64, Dynamic, Dynamic>>;
+pub type Vector = nalgebra::base::Vector<f64, Dynamic, VecStorage<f64, Dynamic, U1>>;
 
 pub trait Mean {
     fn mean(&self, x: &[f64]) -> f64;
@@ -15,18 +16,27 @@ pub trait Kernel {
     fn kernel(&self, x0: &[f64], x1: &[f64]) -> f64;
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Means {
-    means: Vec<f64>,
+    means: Vector,
+}
+impl Means {
+    pub fn as_slice(&self) -> &[f64] {
+        self.means.as_slice()
+    }
+
+    pub fn as_inner(&self) -> &Vector {
+        &self.means
+    }
 }
 
 // NOTE: Must be a positive semi-definite matrix
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Covariance {
     matrix: Matrix,
 }
 impl Covariance {
-    pub fn cholesky(&self) -> Option<Cholesky<f64, Dynamic>> {
-        Cholesky::new(self.matrix.clone())
+    pub fn cholesky(self) -> Option<Cholesky<f64, Dynamic>> {
+        Cholesky::new(self.matrix)
     }
 }
